588216ed586dd5a68f8a1c366bbc78806f5c158b
Chantel Wetzel <chantel.wetzel@noaa.gov>
2020-08-10 16:09:20 -0700

    fix: Remove the specific call for a custom profile parameter

--- R/run_diagnostics.R
+++ R/run_diagnostics.R
@@ -1,7 +1,7 @@
-#' 
-#'
-#' 
-#' 
+#' Wrapper to run each of the 3 standard diagnostic items:
+#' 1) Jitter
+#' 2) Profiles across female m, steepness, and R0
+#' 3) Retrospectives
 #' 
 #'
 #' @param mydir Directory where model files are located.
@@ -11,26 +11,16 @@
 #' @return A vector of likelihoods for each jitter iteration.
 #' @export
 
-run_diagnostics <- function(mydir, 
-							              model_settings ){
+run_diagnostics <- function(mydir, model_settings ){
 
   # Determine working directory on start and return upon exit
-  startdir <- getwd()
-  on.exit(setwd(startdir))
+  #startdir <- getwd()
+  #on.exit(setwd(startdir))
 
-  # determine operating system in a relatively brute force way
-  # OS <- "Mac" # don't know the version$os info for Mac
-  # if(length(grep("linux",version$os)) > 0){
-  #   OS <- "Linux"
-  # }
-  # if(length(grep("mingw",version$os)) > 0){
-  #   OS <- "Windows"
-  # }
-
-  setwd(file.path(mydir, model_settings$base_name))
-  if (length(dir(pattern = paste0("^",model_settings$model,"$","|",model_settings$model,"\\.exe"))) != 1) {
-    stop(model_settings$model, " was not found in ", paste0(mydir, "/", model_settings$base_name), call. = FALSE)
-  }
+  #setwd(file.path(mydir, model_settings$base_name))
+  #if (length(dir(pattern = paste0("^",model_settings$model,"$","|",model_settings$model,"\\.exe"))) != 1) {
+  #  stop(model_settings$model, " was not found in ", paste0(mydir, "/", model_settings$base_name), call. = FALSE)
+  #}
 
 
   if ("jitter" %in% model_settings$run){
@@ -41,10 +31,9 @@ run_diagnostics <- function(mydir,
 	   profile_wrapper(mydir = mydir, model_settings = model_settings)
   }
 
-  if ("profile_custom" %in% model_settings$run){
-     para = model_settings$profile_para[!(model_settings$profile_para %in% c("female_m", "h", "r0"))]
-     profile_wrapper(mydir = mydir, model_settings = model_settings, custom_para = para)
-  }
+  #if (!is.null(model_settings$profile_custom)){
+  #   profile_wrapper(mydir = mydir, model_settings = model_settings, custom_para = model_settings$profile_custom)
+  #}
 
   if ("retro" %in% model_settings$run){
     retro_wrapper(mydir = mydir, model_settings = model_settings)
