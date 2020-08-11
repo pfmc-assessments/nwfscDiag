a608ce732d93a063b0f5b3cb8aebba3402f7b32b
Chantel Wetzel <chantel.wetzel@noaa.gov>
2020-08-11 12:47:11 -0700

    style: Remove unused code.

--- R/run_diagnostics.R
+++ R/run_diagnostics.R
@@ -13,16 +13,6 @@
 
 run_diagnostics <- function(mydir, model_settings ){
 
-  # Determine working directory on start and return upon exit
-  #startdir <- getwd()
-  #on.exit(setwd(startdir))
-
-  #setwd(file.path(mydir, model_settings$base_name))
-  #if (length(dir(pattern = paste0("^",model_settings$model,"$","|",model_settings$model,"\\.exe"))) != 1) {
-  #  stop(model_settings$model, " was not found in ", paste0(mydir, "/", model_settings$base_name), call. = FALSE)
-  #}
-
-
   if ("jitter" %in% model_settings$run){
   	jitter_wrapper(mydir = mydir, model_settings = model_settings)	
   }
@@ -30,10 +20,6 @@ run_diagnostics <- function(mydir, model_settings ){
   if ("profile" %in% model_settings$run){
 	   profile_wrapper(mydir = mydir, model_settings = model_settings)
   }
-
-  #if (!is.null(model_settings$profile_custom)){
-  #   profile_wrapper(mydir = mydir, model_settings = model_settings, custom_para = model_settings$profile_custom)
-  #}
 
   if ("retro" %in% model_settings$run){
     retro_wrapper(mydir = mydir, model_settings = model_settings)
