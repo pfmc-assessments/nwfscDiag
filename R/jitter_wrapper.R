bd8ebd0a83be9a0ae362fd8a391be0aaa3771df8
Chantel Wetzel <chantel.wetzel@noaa.gov>
2020-08-11 12:48:11 -0700

    fix: Remove the deltaLL because not working

--- R/jitter_wrapper.R
+++ R/jitter_wrapper.R
@@ -55,34 +55,35 @@ jitter_wrapper <- function(mydir,  model_settings){
   	base <- r4ss::SS_output(file.path(mydir, model_settings$base_name), covar = FALSE,
   							printstats = FALSE, verbose = FALSE)
 	est  <- base$likelihoods_used[1, 1]
-	like <- profilesummary$likelihoods[1, keys]
+	like <- as.numeric(profilesummary$likelihoods[1, keys])
 	ymax <- as.numeric(quantile(profilesummary$likelihoods[1, keys], 0.80))
 	ymin <- 0 
 
-	#pngfun(wd = jitter_dir, file = paste0("Jitter_", model_settings$jitter_fraction, '.png'), h = 12, w = 9)
-	#plot(keys, like, ylim = c(ymin, ymax), ylab="-log-likelihood", xlab = "Iteration")
-	#abline(h = est, col = 'red', lwd = 2)
-	#abline(h =  min(like), col = 1, lty = 2)
-	#dev.off()
+	pngfun(wd = jitter_dir, file = paste0("Jitter_", model_settings$jitter_fraction, '.png'), h = 12, w = 9)
+	plot(keys, like, ylim = c(ymin, ymax), ylab="-log-likelihood", xlab = "Iteration")
+	abline(h = est, col = 'red', lwd = 2)
+	abline(h =  min(like), col = 1, lty = 2)
+	dev.off()
 
 	# get output
-	#outputs <- profilemodels
-	#quants  <- lapply(outputs, "[[", "derived_quants")
-	#status  <- sapply(sapply(outputs, "[[", "parameters", simplify = FALSE), "[[", "Status")
-	#bounds  <- apply(status, 2, function(x) rownames(outputs[[1]]$parameters)[x %in% c("LO", "HI")])
-	#out     <- data.frame("run" = gsub("replist", "", names(outputs)),
-	#  					  "likelihood" = sapply(sapply(outputs, "[[", "likelihoods_used", simplify = FALSE), "[", 1, 1),
-	#  					  "gradient" = sapply(outputs, "[[", "maximum_gradient_component"),
-	#  					  "SB0" = sapply(quants, "[[", "SSB_Virgin", "Value"),
-	#  					  "SBfinal" = sapply(quants, "[[", paste0("SSB_", profilesummary$endyrs[1]), "Value"),
-	#  					  #"Fmsy" = sapply(quants, "[[", "annF_MSY", "Value"),
-	#  					  "Nparsonbounds" = apply(status, 2, function(x) sum(x %in% c("LO", "HI"))),
-	#  					  stringsAsFactors = FALSE)
-#
-	#out[, "deltaNLL"] <- out[, "likelihood"] - out[row.names(out) == "replist0", "likelihood"]
-	## write tables
-	#write.csv(x = table(unlist(bounds)), file = file.path(jitter_dir, "jitter_parsonbounds.csv"), row.names = FALSE)
-	#write.csv(x = out, file = file.path(jitter_dir, "jitter_results.csv"), row.names = FALSE)
+	outputs <- profilemodels
+	quants  <- lapply(outputs, "[[", "derived_quants")
+	status  <- sapply(sapply(outputs, "[[", "parameters", simplify = FALSE), "[[", "Status")
+	bounds  <- apply(status, 2, function(x) rownames(outputs[[1]]$parameters)[x %in% c("LO", "HI")])
+	out     <- data.frame("run" = gsub("replist", "", names(outputs)),
+	  					  "likelihood" = sapply(sapply(outputs, "[[", "likelihoods_used", simplify = FALSE), "[", 1, 1),
+	  					  #"deltaNLL" = as.numeric(sapply(sapply(outputs, "[[", "likelihoods_used", simplify = FALSE), "[", 1, 1)) - est,
+	  					  "gradient" = sapply(outputs, "[[", "maximum_gradient_component"),
+	  					  "SB0" = sapply(quants, "[[", "SSB_Virgin", "Value"),
+	  					  "SBfinal" = sapply(quants, "[[", paste0("SSB_", profilesummary$endyrs[1]), "Value"),
+	  					  #"Fmsy" = sapply(quants, "[[", "annF_MSY", "Value"),
+	  					  "Nparsonbounds" = apply(status, 2, function(x) sum(x %in% c("LO", "HI"))),
+	  					  stringsAsFactors = FALSE)
+	
+	#out[, "deltaNLL"] <- out[, "likelihood"] - est
+	# write tables
+	write.csv(x = table(unlist(bounds)), file = file.path(jitter_dir, "jitter_parsonbounds.csv"), row.names = FALSE)
+	write.csv(x = out, file = file.path(jitter_dir, "jitter_results.csv"), row.names = FALSE)
 
 	message("Finished jitters.")
 	
