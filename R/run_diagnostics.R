cacfa64f881ffd723dcd728a85dc565e8aceb8ac
Chantel Wetzel <chantel.wetzel@noaa.gov>
2020-09-02 11:07:03 -0700

    fix: load plyr package

--- R/run_diagnostics.R
+++ R/run_diagnostics.R
@@ -15,7 +15,8 @@ run_diagnostics <- function(mydir, model_settings ){
 
   library(r4ss)
   library(HandyCode)
-  
+  library(plyr)
+
   if ("jitter" %in% model_settings$run){
   	jitter_wrapper(mydir = mydir, model_settings = model_settings)	
   }
