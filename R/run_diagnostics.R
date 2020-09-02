2e2895ad2cddc7275ae1d9c8d7ca52b900de1f36
Chantel Wetzel <chantel.wetzel@noaa.gov>
2020-09-02 11:02:56 -0700

    fix: load packages

--- R/run_diagnostics.R
+++ R/run_diagnostics.R
@@ -13,7 +13,9 @@
 
 run_diagnostics <- function(mydir, model_settings ){
 
-
+  library(r4ss)
+  library(HandyCode)
+  
   if ("jitter" %in% model_settings$run){
   	jitter_wrapper(mydir = mydir, model_settings = model_settings)	
   }
