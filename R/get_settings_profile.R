15d0244f9b23ae4cdb9639a528dcfcd690cffb47
Chantel Wetzel <chantel.wetzel@noaa.gov>
2020-09-04 10:19:13 -0700

    add: add details on param_space and examples

--- R/get_settings_profile.R
+++ R/get_settings_profile.R
@@ -8,10 +8,11 @@
 #' should be interpretted as relative to the base model estimate vs. across a pre-specified range.
 #' An example is for R0 where the default setting below indicates that the param_space is relative
 #' where the low bound for the profile is set = base model log(R0) - 2 and high = base model log(R0) + 2.
-#' The default range for M is set in relative space to explore a range of (M - 0.40 * M) - (M + 0.40 * M) 
+#' The default range for M is set as a multiplier to explore a range of (M - 0.40 * M) - (M + 0.40 * M) 
 #' at a step size of 0.005. This range may be too large (or small) with a step size too large (or too small)
-#' and should be considered if the default settings are appropriate for your specific model. One also could
-#' opt to define the range in 'real' space which could be selected in the "param_space" column of the matrix.
+#' and should be considered if the default settings are appropriate for your specific model. The default setting
+#' for steepness is in 'real' space which means that the low and high is in the same parameter space as the 
+#' parameter. A user can select any of the options for specifying a parameter range for any parameter. 
 #' 
 #' @param parameters vector of SS parameter names to conduct a profile for
 #' @param low a vector of low paramater bounds for the profile
@@ -19,7 +20,7 @@
 #' @param step_size increments to run the profile between the low and high bounds
 #' @param param_space options: real, mulitplier, relative indicates how to interpret the low and high bound values.
 #' real indicates bounds in the parameter space, relative indicates how far to go from the base parameter, and
-#' multiplier indicates that low and high bounds are set at x% above and below the base parameter.
+#' multiplier indicates that low and high bounds are set at x% above and below the base parameter (i.e. (M - 0.40 * M) - (M + 0.40 * M)).
 #'
 #' @return A matrix of low, high, and step size values for the default parameters
 #' that should be profiled. The goal is to provide users with a template
@@ -27,10 +28,22 @@
 #' the default ones.
 #'
 #' @examples
-#' Use the default settings
-#' get_settings_profile()
+#' \dontrun{
 #' 
+#' # Define each parameter in real space
+#' get_settings_profile( parameters =  c("NatM_p_1_Fem_GP_1", "SR_BH_steep", "SR_LN(R0)"),
+#						 low =  c(0.02, 0.25, 8),
+#						 high = c(0.07, 1.0,  11),
+#						 step_size = c(0.005, 0.05, 0.25),
+#						 param_space = c('real', 'real', 'real')) 
 #' 
+#' # Example 2: Run a profile for natural mortality alone
+#' get_settings_profile( parameters =  c("NatM_p_1_Fem_GP_1"),
+#'						 low =  c(0.40),
+#'						 high = c(0.40),
+#'						 step_size = c(0.005),
+#'						 param_space = c('multiplier')) 
+#'}
 #'
 get_settings_profile <- function( parameters =  c("NatM_p_1_Fem_GP_1", "SR_BH_steep", "SR_LN(R0)"),
 								  low =  c(0.40, 0.25, -2),
