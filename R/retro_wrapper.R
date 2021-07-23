432a3a18832f89a793e88b91913710ee64cdb85b
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2021-07-23 05:50:08 -0700

    feat(retro_wrapper): save more output to the disk for inclusion in documents
    * Updated the documentation to explicitly list what is saved and how to use it
    * stopped output from being saved to a separate file when copying files close #7
    * Add text to r4ss figures

--- R/retro_wrapper.R
+++ R/retro_wrapper.R
@@ -1,29 +1,62 @@
-#' Retrospecitive runs
-#' To be called from the run_diagnostics function after creating
-#' the model settings using the get_settings function.
-#' 
+#' Run [r4ss::SS_doRetro] based on `model_settings`
+#'
+#' Create a folder containing retrospective runs for a given base-model folder.
+#' Runs are created using settings specified in `model_settings` and
+#' is formatted for inclusion in a document.
+#'
+#' @seealso The following functions interact with `retro_wrapper`:
+#' * [run_diagnostics]: calls `retro_wrapper`
+#' * [r4ss::SS_doRetro]: the workhorse of `retro_wrapper` that does the peels
 #'
 #' @template mydir
 #' @template model_settings
 #' 
-#' @author Chantel Wetzel.
+#' @author Chantel Wetzel
+#' @return
+#' Nothing is explicitly returned from `retro_wrapper`.
+#' The following objects are saved to the disk.
+#'
+#' * `mohnsrho.csv` with the following columns:
+#'   * type: the type of Mohn's rho
+#'     * [Hurtado-Ferro et al. (2015)](https://doi.org/10.1093/icesjms/fsu198),
+#'     * [Northeast Fisheries Science Center (NEFSC)](https://www.fisheries.noaa.gov/about/northeast-fisheries-science-center), and
+#'     * [Alaska Fisheries Science Center (AFSC)](https://www.fisheries.noaa.gov/about/alaska-fisheries-science-center)
+#'   * Quantity: the stock assessment quantity of interest
+#'   * values: the Mohn's rho values
+#' * A set of figures from [r4ss::SSplotComparisons]
+#' * `retrofigures4doc.csv` for use with `sa4ss::add_figure` to add retro peels of
+#'   spawning stock biomass (_SSB_) and fraction unfished,
+#'   complete with captions and alternative text.
+#'
+#'   `apply(utils::read.csv(file.path("..", paste0(mod_loc, "_retro"), "retrofigures4doc.csv")), 1, function(x) do.call(sa4ss::add_figure, as.list(x)))`
+#'
+#' * `mohnsrho.tex` for use with `sa4ss::read_child()`
+#'   inside of an environment with `results = "asis"`
+#'   to include a table of Mohn's rho values in a document.
+#'
+#'   `sa4ss::read_child(file.path(paste0(params$model, "_retro"), "mohnsrho.tex"))`
+#'
+#' * `retro_output.Rdata` with a list of R objects.
+#'
 #' @export
 
 retro_wrapper <- function(mydir,  model_settings){
 
-	if(!file.exists(file.path(mydir, model_settings$base_name, "Report.sso"))) {
-    	message("There is no Report.sso file in the base model directory", file.path(mydir, model_settings$base_name))
-    	stop()
-  	}
+  if(!file.exists(file.path(mydir, model_settings$base_name, "Report.sso"))) {
+    message("There is no Report.sso file in the base model directory", file.path(mydir, model_settings$base_name))
+    stop()
+  }
 
 	# Create a jitter folder with the same naming structure as the base model
 	retro_dir <- file.path(mydir, paste0(model_settings$base_name, "_retro"))
   dir.create(retro_dir, showWarnings = FALSE)
   all_files = list.files(file.path(mydir, model_settings$base_name)) 
-  capture.output(file.copy(from = file.path(mydir, model_settings$base_name, all_files), 
-  			  to = retro_dir, 
-  			  overwrite = TRUE), file = "run_diag_warning.txt")
-  message("Running retrospecitives")
+  ignore <- file.copy(
+    from = file.path(mydir, model_settings$base_name, all_files),
+    to = retro_dir,
+    overwrite = TRUE
+  )
+  message("Running retrospectives.")
 
   r4ss::SS_doRetro(masterdir = retro_dir, 
   				         overwrite = model_settings$overwrite,
@@ -36,7 +69,7 @@ retro_wrapper <- function(mydir,  model_settings){
                    CallType = model_settings$CallType,
                    RemoveBlocks = model_settings$RemoveBlocks)
 
-  capture.output(file.remove(from = file.path(retro_dir, all_files)), file = "run_diag_warning.txt")
+  ignore <- file.remove(from = file.path(retro_dir, all_files))
 
   runs <- list()
   for(aa in 1:(length(model_settings$retro_yrs) + 1)){
@@ -47,23 +80,142 @@ retro_wrapper <- function(mydir,  model_settings){
   	}
   }
 
-	retroSummary <- r4ss::SSsummarize(runs)
+  retroSummary <- r4ss::SSsummarize(runs, verbose = FALSE)
 	endyrvec <- c(retroSummary$endyrs[1], retroSummary$endyrs[1] + model_settings$retro_yrs)
+
+  # Calculate Mohn's rho
+  rhosall <- mapply(
+    r4ss::SSmohnsrho,
+    summaryoutput = lapply(
+      seq_along(runs)[-1],
+      function(x) r4ss::SSsummarize(runs[1:x], verbose = FALSE)
+    ),
+    endyrvec = mapply(seq,from=endyrvec[1], to= endyrvec[-1]),
+    startyr = endyrvec[-1]
+  )
+  rhos <-   rhosall %>%
+    data.frame %>%
+    dplyr::select(values = NCOL(rhosall)) %>%
+    tibble::rownames_to_column("ind") %>%
+    dplyr::mutate(
+      ind = gsub("\\.all$", "", ind),
+      Quantity = gsub("[A-Za-z_]+_([A-Za-z]+$)|(^[A-Za-z]+$)", "\\1\\2", ind),
+      Quantity = gsub("Bratio", "Fraction unfished", Quantity),
+      Quantity = gsub("Rec", "Recruitment", Quantity),
+      ind = gsub("_[A-Za-z]+$|^[A-Za-z]+$", "", ind),
+      ind = gsub("^$", "Hurtado-Ferro", ind),
+      ind = gsub("WoodHole", "NEFSC", ind),
+      ind = gsub("_Hurtado", "", ind),
+    ) %>%
+    dplyr::rename(type = "ind") %>%
+    dplyr::select(type, Quantity, values)
+  utils::write.csv(
+    x = rhos,
+    file = file.path(retro_dir, "mohnsrho.csv")
+  )
 
   retro_output <- list()
   retro_output$plotdir <- retro_dir
   retro_output$endyrvec <- endyrvec
   retro_output$retroSummary <- retroSummary
   retro_output$model_settings <- model_settings
+  retro_output$rhosall <- rhosall
+  retro_output$rhos <- rhos
   save(retro_output, file = file.path(retro_dir, "retro_output.Rdata"))
 
-	r4ss::SSplotComparisons(retroSummary, 
-		 			                endyrvec = endyrvec, 
-		 			                legendlabels = c("Base Model", paste("Data", model_settings$retro_yrs, "Years")),
-		 			                plotdir = retro_dir, 
-    				              legendloc = "topright", 
-    				              print = TRUE, 
-    				              pdf = FALSE)
+  # Make figures, copy over two figures with ones that have Mohn's rho values
+	r4ss::SSplotComparisons(
+    summaryoutput = retroSummary,
+    endyrvec = endyrvec,
+    legendlabels = c(
+      "Base Model",
+      sprintf("Data %.0f year%s",
+        model_settings$retro_yrs,
+        ifelse(abs(model_settings$retro_yrs) == 1, "", "s")
+      )
+    ),
+    plotdir = retro_dir,
+    legendloc = "topright",
+    print = TRUE,
+    pdf = FALSE
+  )
+  savedplotinfo <- mapply(
+    FUN = r4ss::SSplotComparisons,
+    MoreArgs = list(
+      summaryoutput = retroSummary,
+      endyrvec = endyrvec,
+      legendloc = "topleft",
+      plotdir = retro_dir,
+      print = TRUE, plot = FALSE, pdf = FALSE
+    ),
+    subplot = c(2, 4),
+    legendlabels = lapply(
+      c("SSB", "Bratio"),
+      function(x) {
+      c(
+        "Base Model",
+        sprintf("Data %.0f year%s (Mohn's rho %.2f)",
+          model_settings$retro_yrs,
+          ifelse(abs(model_settings$retro_yrs) == 1, "", "s"),
+          rhosall[rownames(rhosall) == x, ]
+        )
+      )
+    })
+  )
+  
+  utils::write.csv(
+    x = data.frame(
+      caption = paste(
+        "Retrospective patterns for", 
+        c("spawning stock biomass (\\emph{SSB})", "fraction unfished"),
+        "when up to", xfun::numbers_to_words(max(abs(model_settings$retro_yr))),
+        "years of data were removed from the base model.",
+        "Mohn's rho (Mohn, 1999) values, as calculated in Hurtado-Ferro et al. (2015), were",
+        "recalculated for each peel given the removal of another year of data.",
+        "See Table \\ref{tab:RetroMohnsrho} for other derivations of Mohn's rho."
+      ),
+      alt_caption = sprintf("Each successive peel of data led to a Mohn's rho of %s for %s.",
+        lapply(c("SSB", "Bratio"), function(x) {
+          knitr::combine_words(sprintf("%.2f", (rhosall[rownames(rhosall) == x, ])))
+        }),
+        c("SSB", "fraction unfished")
+      ),
+      label = c("RetroSsb", "RetroFractionunfished"),
+      filein = file.path("..", retro_dir, c("compare2_spawnbio_uncertainty.png", "compare4_Bratio_uncertainty.png"))
+    ),
+    file = file.path(retro_dir, "retrofigures4doc.csv"),
+    row.names = FALSE
+  )
+
+  # Make table for document with caption
+  rhos %>%
+    tidyr::spread(key = "type", value = "values") %>%
+    dplyr::mutate(
+      Quantity = gsub("(^[FSB]+$)", "\\\\emph{\\1}", Quantity),
+    ) %>%
+    kableExtra::kbl(
+      format = "latex", booktabs = TRUE, digits = 2, longtable = TRUE,
+      label = "RetroMohnsrho",
+      escape = FALSE,
+      caption = paste(
+        "The average relative bias of retrospective estimates",
+        "(Mohn's rho; Mohn, 1999) given the removal of",
+        xfun::numbers_to_words(max(abs(model_settings$retro_yr))),
+        "years of data for",
+        "fishing intensity (\\emph{F}),",
+        "fraction unfished (Figure \\ref{fig:RetroFractionunfished}),",
+        " recruitment, and",
+        "spawning stock biomass (\\emph{SSB}; Figure \\ref{fig:RetroSsb}).",
+        "Columns are derivations of Mohn's rho used by the",
+        "Alaska Fisheries Science Center (AFSC),",
+        "suggested by Hurtado-Ferro et al. (2015), and",
+        "used by the Northeast Fisheries Science Center (NEFSC)."
+      )
+    ) %>%
+    kableExtra::kable_classic(full_width = FALSE) %>%
+    kableExtra::save_kable(
+      file = file.path(retro_dir, "mohnsrho.tex")
+    )
 
 	message("Finished retrospective runs.")
 }
