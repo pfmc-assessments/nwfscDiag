b9c9d3177e5218b1eb6cec210d7cbe0c6ac50e16
Chantel Wetzel <chantelwetzel@noaa.gov>
2023-07-17 12:49:48 -0700

    fix: add packages to calls

--- R/retro_wrapper.R
+++ R/retro_wrapper.R
@@ -235,20 +235,20 @@ retro_wrapper <- function(mydir,  model_settings) {
   denom <- paste0("model", 1:n)
   base <- as.symbol("model1")
   sb <- retroSummary$SpawnBio %>%
-      subset(Yr %in% years) %>%
-      mutate_at(vars(denom), list(per_diff = ~100*./model1 - 100)) 
+      dplyr::filter(Yr %in% years) %>%
+      dplyr::mutate_at(dplyr::vars(denom), list(per_diff = ~100*./model1 - 100)) 
   sb$Reference_Point <- label
   bratio <- retroSummary$Bratio %>%
-      subset(Yr %in% years) %>%
-      mutate_at(vars(denom), list(per_diff = ~100*./model1 - 100)) 
+      dplyr::filter(Yr %in% years) %>%
+      dplyr::mutate_at(dplyr::vars(denom), list(per_diff = ~100*./model1 - 100)) 
   bratio$Reference_Point <- "Fraction Unfished"
   f <- retroSummary$Fvalue %>%
-      subset(Yr %in% years) %>%
-      mutate_at(vars(denom), list(per_diff = ~100*./model1 - 100)) 
+      dplyr::filter(Yr %in% years) %>%
+      dplyr::mutate_at(dplyr::vars(denom), list(per_diff = ~100*./model1 - 100)) 
   f$Reference_Point <- "F"
   rec <- retroSummary$recruits %>%
-      subset(Yr %in% years) %>%
-      mutate_at(vars(denom), list(per_diff = ~100*./model1 - 100)) 
+      dplyr::filter(Yr %in% years) %>%
+      dplyr::mutate_at(dplyr::vars(denom), list(per_diff = ~100*./model1 - 100)) 
   rec$Reference_Point <- "Recruits"
 
   col_names <- c("Yr", "Reference_Point", paste0("model", 1:n, "_per_diff"))
@@ -274,40 +274,40 @@ retro_wrapper <- function(mydir,  model_settings) {
   leg_order <- c("Base Model", paste0("Retro -", 1:(length(endyrvec) -1)))
   df_out$Run <- factor(df_out$Run, levels = leg_order)
   xrange <- c(ifelse(min(df_out$Yr) < 1980, 1980, min(df_out$Yr)), max(df_out$Yr))
-  find <- df_out %>% subset(Yr >= xrange[1]) %>% 
-    summarize(
+  find <- df_out %>% dplyr::filter(Yr >= xrange[1]) %>% 
+    dplyr::summarize(
       bound = abs(max(diff)))
   yrange <- c(-1 * find$bound - 5, find$bound + 5)
 
-  ggplot(df_out, aes(x = Yr, y = diff, col = Run)) +
-      geom_line() +
-      geom_point() +
-      ylim(yrange) + 
-      scale_x_continuous(limits = xrange, expand = c(0,0)) + 
-      ylab("% Differece from Base Model") +
-      xlab("Year") +
-      scale_colour_viridis_d() +
-      theme_bw(base_size = 15) + 
-      facet_wrap("Reference_Point")
-  ggsave(filename = file.path(retro_dir, "retro_percent_difference_4_panel.png"), width = 10, height = 12)
+  ggplot2::ggplot(df_out, ggplot2::aes(x = Yr, y = diff, col = Run)) +
+      ggplot2::geom_line() +
+      ggplot2::geom_point() +
+      #ylim(yrange) + 
+      ggplot2::scale_x_continuous(limits = xrange, expand = c(0,0)) + 
+      ggplot2::ylab("% Differece from Base Model") +
+      ggplot2::xlab("Year") +
+      ggplot2::scale_colour_viridis_d() +
+      ggplot2::theme_bw(base_size = 15) + 
+      ggplot2::facet_wrap("Reference_Point", scales = 'free_y')
+  ggplot2::ggsave(filename = file.path(retro_dir, "retro_percent_difference_4_panel.png"), width = 10, height = 12)
 
   sub_out <- df_out[df_out$Reference_Point != "Recruits",]
-  find <- sub_out %>% subset(Yr >= xrange[1]) %>% 
-    summarize(
+  find <- sub_out %>% dplyr::filter(Yr >= xrange[1]) %>% 
+    dplyr::summarize(
       bound = abs(max(diff)))
   yrange <- c(-1 * find$bound - 5, find$bound + 5)
   
-  ggplot(sub_out, aes(x = Yr, y = diff, col = Run)) +
-      geom_line() +
-      geom_point() +
-      ylim(yrange) + 
-      scale_x_continuous(limits = xrange, expand = c(0,0)) + 
-      ylab("% Differece from Base Model") +
-      xlab("Year") +
-      scale_colour_viridis_d() +
-      theme_bw(base_size = 15) + 
-      facet_wrap("Reference_Point", ncol = 1, nrow = 3)
-  ggsave(filename = file.path(retro_dir, "retro_percent_difference_3_panel.png"), width = 10, height = 12)
+  ggplot2::ggplot(sub_out, ggplot2::aes(x = Yr, y = diff, col = Run)) +
+      ggplot2::geom_line() +
+      ggplot2::geom_point() +
+      ggplot2::ylim(yrange) + 
+      ggplot2::scale_x_continuous(limits = xrange, expand = c(0,0)) + 
+      ggplot2::ylab("% Differece from Base Model") +
+      ggplot2::xlab("Year") +
+      ggplot2::scale_colour_viridis_d() +
+      ggplot2::theme_bw(base_size = 15) + 
+      ggplot2::facet_wrap("Reference_Point", ncol = 1, nrow = 3)
+  ggplot2::ggsave(filename = file.path(retro_dir, "retro_percent_difference_3_panel.png"), width = 10, height = 12)
   
   endyrvec_long <- rev((min(endyrvec)-2):(max(endyrvec)))
   pngfun(wd = retro_dir, file = "recruitment_retrospective_squid.png", h = 7, w = 7)
