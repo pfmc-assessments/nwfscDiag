4784d7729f9fc78e6b9352cc352e729adb2d45a1
e-gugliotti-NOAA <elizabeth.gugliotti@noaa.gov>
2024-04-10 11:18:34 -0400

    try r4ss check_exe()

--- R/profile_wrapper.R
+++ R/profile_wrapper.R
@@ -39,11 +39,13 @@ profile_wrapper <- function(mydir, model_settings) {
     f(x / accuracy) * accuracy
   }
 
-  check_exe <- model_settings$exe
-  # check whether exe is in directory
-  if (!check_exe %in% list.files(file.path(mydir, model_settings$base_name))) {
-    stop("Executable not found in ", file.path(mydir, model_settings$base_name))
-  }
+  # check_exe <- model_settings$exe
+  # # check whether exe is in directory
+  # if (!check_exe %in% list.files(file.path(mydir, model_settings$base_name))) {
+  #   stop("Executable not found in ", file.path(mydir, model_settings$base_name))
+  # }
+
+  r4ss::check_exe(file.path(mydir, model_settings$base_name))
 
   N <- nrow(model_settings$profile_details)
 
