4c8612e49df887e2dda3410e1252e4c693e8d0a7
e-gugliotti-NOAA <elizabeth.gugliotti@noaa.gov>
2024-04-10 13:34:41 -0400

    explicitly state exe and dir

--- R/profile_wrapper.R
+++ R/profile_wrapper.R
@@ -39,7 +39,7 @@ profile_wrapper <- function(mydir, model_settings) {
     f(x / accuracy) * accuracy
   }
 
-  r4ss::check_exe(file.path(mydir, model_settings$base_name, model_settings$exe))
+  r4ss::check_exe(exe = model_settings$exe, dir = file.path(mydir, model_settings$base_name))
 
   N <- nrow(model_settings$profile_details)
 
