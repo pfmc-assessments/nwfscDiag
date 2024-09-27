291e2a212e87f4fa75933a65d7195b8eba3a1ef3
Chantel Wetzel <chantel.wetzel@noaa.gov>
2024-09-27 12:41:28 -0700

    fix tests

--- tests/testthat/test-runs.R
+++ tests/testthat/test-runs.R
@@ -4,7 +4,6 @@
 # devtools::test()
 #
 # This is the directory where I want the tests to specifically run
-#setwd("C:/Users/Chantel.Wetzel/Documents/GitHub/nwfscDiag/tests") 
 tmp_path <- file.path("/test-runs-output")
 dir.create(tmp_path, "test-runs-output", showWarnings = TRUE)
 
@@ -14,46 +13,49 @@ runs_path <- system.file("extdata", package = "nwfscDiag")
 # clean up (Comment out this if  you want to keep the files created by the tests)
 on.exit(unlink(tmp_path, recursive = TRUE))
 
-test_path <- file.path(runs_path, "simple")
+test_path <- file.path(runs_path, "simple_small")
 skip_test <- TRUE
 if(.Platform$OS.type == "windows") {
-	if (file.exists(file.path(test_path, "ss.exe"))) {
+	if (file.exists(file.path(test_path, "ss3.exe"))) {
 		skip_test <- FALSE
 	}
 }
 if(.Platform$OS.type == "unix") {
-	if (file.exists(file.path(test_path, "ss"))) {
+	if (file.exists(file.path(test_path, "ss3"))) {
 		skip_test <- FALSE
 	}
 }
 
 test_that("Do profile using the simple model", {
 
-   #skip_if(skip_test == TRUE, message = "SS executable missing")
-   path <- file.path(runs_path)
+  path <- file.path(runs_path)
 
-	get <- get_settings_profile(parameters =  c("NatM_uniform_Fem_GP_1", "SR_BH_steep", "SR_LN(R0)"),
-								low =  c(0.20, 0.40, -0.5),
-								high = c(0.20, 1.0,  0.5),
-								step_size = c(0.01, 0.1, 0.25),
-								param_space = c('multiplier', 'real', 'relative'))
+	get <- get_settings_profile(
+	  parameters =  c("NatM_uniform_Fem_GP_1", "SR_BH_steep", "SR_LN(R0)"),
+		low =  c(0.20, 0.40, -0.5),
+		high = c(0.20, 1.0,  0.5),
+		step_size = c(0.01, 0.2, 0.25),
+		param_space = c('multiplier', 'real', 'relative'))
 
-	model_settings <- get_settings(settings = list(base_name = "simple",
-											  run = c("profile"),
-											  profile_details = get ))
+	model_settings <- get_settings(
+	  settings = list(
+	    base_name = "simple_small",
+		  run = c("profile"),
+		  profile_details = get )
+	  )
 
 	run_diagnostics(mydir = path, model_settings = model_settings)
 
 	profile_para <- c("NatM_uniform_Fem_GP_1", "SR_BH_steep", "SR_LN(R0)")
 	check <- 0
 	for (a in 1:length(profile_para)){
-		check <- check + 
+		check <- check +
 			as.numeric(
 				file.exists(
-					file.path(path, 
-						paste0(model_settings$base_name, "_profile_", 
+					file.path(path,
+						paste0(model_settings$base_name, "_profile_",
 						profile_para[a]),
-						paste0("profile_", profile_para[a], "_quant_table.csv")))
+						paste0(profile_para[a], "_quant_table.csv")))
 			)
 	}
 	expect_true(check == length(profile_para))
@@ -61,19 +63,19 @@ test_that("Do profile using the simple model", {
 
 
 test_that("Do jitters using the simple model", {
- 
-    #skip_if(skip_test == TRUE, message = "SS executable missing")
-    path <- file.path(runs_path)
 
-	model_settings <- get_settings(settings = list(base_name = "simple",
-												   run = c("jitter"),
-												   Njitter = 3))
+  path <- file.path(runs_path)
+
+	model_settings <- get_settings(
+	  settings = list(base_name = "simple_small",
+		run = c("jitter"),
+		Njitter = 3))
 
 	run_diagnostics(mydir = path, model_settings = model_settings)
 
-	jitter_folder <- file.path(path, 
-						paste0(model_settings$base_name, "_jitter_", 
-							   model_settings$jitter_fraction))
+	jitter_folder <- file.path(
+	  path,
+    paste0(model_settings$base_name, "_jitter_", model_settings$jitter_fraction))
 
 	expect_true(file.exists(file.path(jitter_folder, "jitter_results.csv")))
 })
@@ -81,17 +83,16 @@ test_that("Do jitters using the simple model", {
 
 test_that("Do retrospectives using the simple model", {
 
-	#skip_if(skip_test == TRUE, message = "SS executable missing")
-   path <- file.path(runs_path)
+  path <- file.path(runs_path)
 
-	model_settings <- get_settings(settings = list(base_name = "simple",
-											  run = c("retro"),
-											  retro_yrs = -1:-2))
+	model_settings <- get_settings(
+	  settings = list(base_name = "simple_small",
+		run = c("retro"),
+		retro_yrs = -1:-2))
 
 	run_diagnostics(mydir = path, model_settings = model_settings)
-
+  n <- length(model_settings$retro_yrs)
 	check <- file.exists(
-				file.path(path, paste0(model_settings$base_name, "_retro"),
-						  "mohnsrho.csv"))
+	  file.path(path, paste0(model_settings$base_name, "_retro_", n, "_yr_peel"), "mohnsrho.csv"))
 	expect_true(check)
 })
