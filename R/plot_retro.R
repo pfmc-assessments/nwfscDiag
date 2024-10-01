ef530ec04c659fb6574e4339b1ba59379e779c9a
Chantel Wetzel <chantel.wetzel@noaa.gov>
2024-10-01 10:09:18 -0700

    refactor: replace dplyr::vars which is deprecated

--- R/plot_retro.R
+++ R/plot_retro.R
@@ -18,7 +18,6 @@
 #' @export
 
 plot_retro <- function(mydir,  model_settings, output) {
-  #TO DO: find a replacement for dplyr::vars()
   retro_dir <- output[["plotdir"]]
   endyrvec <- output[["endyrvec"]]
   retroSummary <- output[["retroSummary"]]
@@ -119,58 +118,85 @@ plot_retro <- function(mydir,  model_settings, output) {
       })
   )
 
-  label <- ifelse(retroSummary$SpawnOutputUnits[1] == "biomass",
-                  "Spawning Biomass", "Spawning Output")
-  n <- ncol(retroSummary$SpawnBio) - 2
-  years <- retroSummary$startyr[1]:retroSummary$endyr[1] + 1
+  label <- ifelse(
+    retroSummary[["SpawnOutputUnits"]][1] == "biomass",
+    "Spawning Biomass",
+    "Spawning Output"
+    )
+  n <- ncol(retroSummary[["SpawnBio"]]) - 2
+  years <- retroSummary[["startyrs"]][1]:retroSummary[["endyrs"]][1] + 1
   denom <- paste0("model", 1:n)
   base <- as.symbol("model1")
-  sb <- retroSummary$SpawnBio %>%
-    dplyr::filter(Yr %in% years) %>%
-    dplyr::mutate_at(dplyr::vars(denom), list(per_diff = ~100*./model1 - 100))
-  sb$Reference_Point <- label
-  bratio <- retroSummary$Bratio %>%
-    dplyr::filter(Yr %in% years) %>%
-    dplyr::mutate_at(dplyr::vars(denom), list(per_diff = ~100*./model1 - 100))
-  bratio$Reference_Point <- "Fraction Unfished"
-  f <- retroSummary$Fvalue %>%
-    dplyr::filter(Yr %in% years) %>%
-    dplyr::mutate_at(dplyr::vars(denom), list(per_diff = ~100*./model1 - 100))
-  f$Reference_Point <- "F"
-  rec <- retroSummary$recruits %>%
-    dplyr::filter(Yr %in% years) %>%
-    dplyr::mutate_at(dplyr::vars(denom), list(per_diff = ~100*./model1 - 100))
-  rec$Reference_Point <- "Recruits"
+  sb <- retroSummary[["SpawnBio"]] |>
+    dplyr::filter(Yr %in% years) |>
+    dplyr::mutate(dplyr::across(
+      # Finds the columns that match "model1", "model2", ...
+      .cols = dplyr::matches("model[0-9]+$"),
+      # model1 is the reference for all model comparisons
+      .fns = \(x) 100 * x / model1 - 100,
+      # renames the output columns per_diff_model1, per_diff_model2, ...
+      .names = "per_diff_{.col}"
+    ))
+  sb[["Reference_Point"]] <- label
+  bratio <- retroSummary[["Bratio"]] |>
+    dplyr::filter(Yr %in% years) |>
+    dplyr::mutate(dplyr::across(
+      .cols = dplyr::matches("model[0-9]+$"),
+      .fns = \(x) 100 * x / model1 - 100,
+      .names = "per_diff_{.col}"
+    ))
+  bratio[["Reference_Point"]] <- "Fraction Unfished"
+  f <- retroSummary[["Fvalue"]] |>
+    dplyr::filter(Yr %in% years) |>
+    dplyr::mutate(dplyr::across(
+      .cols = dplyr::matches("model[0-9]+$"),
+      .fns = \(x) 100 * x / model1 - 100,
+      .names = "per_diff_{.col}"
+    ))
+  f[["Reference_Point"]] <- "F"
+  rec <- retroSummary[["recruits"]] |>
+    dplyr::filter(Yr %in% years) |>
+    dplyr::mutate(dplyr::across(
+      .cols = dplyr::matches("model[0-9]+$"),
+      .fns = \(x) 100 * x / model1 - 100,
+      .names = "per_diff_{.col}"
+    ))
+  rec[["Reference_Point"]] <- "Recruits"
 
-  col_names <- c("Yr", "Reference_Point", paste0("model", 1:n, "_per_diff"))
-  df <- rbind(sb[ , colnames(sb) %in% col_names],
-              bratio[, colnames(bratio) %in% col_names],
-              f[ , colnames(f) %in% col_names],
-              rec[, colnames(rec) %in% col_names]) %>%
-    tidyr::pivot_longer(cols = starts_with("model"), names_to = "model", values_to = "diff")
+  col_names <- c("Yr", "Reference_Point", paste0("per_diff_model", 1:n))
+  df <- rbind(
+    sb[ , colnames(sb) %in% col_names],
+    bratio[, colnames(bratio) %in% col_names],
+    f[ , colnames(f) %in% col_names],
+    rec[, colnames(rec) %in% col_names]) |>
+    tidyr::pivot_longer(
+      cols = starts_with("per_diff_model"),
+      names_to = "model",
+      values_to = "diff"
+    )
 
   df_out <- NULL
   y <- years
   for (a in 1:n){
-    col_name <- paste0("model", a, "_per_diff")
-    df_out <- rbind(df_out, df[df$Yr %in% y & df$model %in% col_name, ])
+    col_name <- paste0("per_diff_model", 1:n)
+    df_out <- rbind(df_out, df[df[["Yr"]] %in% y & df[["model"]] %in% col_name, ])
     if (a == 1){
-      df_out$model[df_out$model == col_name] <- "Base Model"
+      df_out[["model"]][df_out[["model"]] == col_name] <- "Base Model"
     } else {
-      df_out$model[df_out$model == col_name] <- paste0("Retro -", a-1)
+      df_out[["model"]][df_out[["model"]] == col_name] <- paste0("Retro -", a-1)
     }
     y <- y - 1
   }
   colnames(df_out)[colnames(df_out) == "model"] <- "Run"
   leg_order <- c("Base Model", paste0("Retro -", 1:(length(endyrvec) -1)))
-  df_out$Run <- factor(df_out$Run, levels = leg_order)
-  xrange <- c(ifelse(min(df_out$Yr) < 1980, 1980, min(df_out$Yr)), max(df_out$Yr))
-  find <- df_out %>%
-    dplyr::filter(Yr >= xrange[1]) %>%
+  df_out[["Run"]] <- factor(df_out[["Run"]], levels = leg_order)
+  xrange <- c(ifelse(min(df_out[["Yr"]]) < 1980, 1980, min(df_out[["Yr"]])), max(df_out[["Yr"]]))
+  find <- df_out |>
+    dplyr::filter(Yr >= xrange[1]) |>
     dplyr::summarize(
       bound = abs(max(diff))
     )
-  yrange <- c(-1 * find$bound - 5, find$bound + 5)
+  yrange <- c(-1 * find[["bound"]] - 5, find[["bound"]] + 5)
 
   ggplot2::ggplot(df_out, ggplot2::aes(x = Yr, y = diff, col = Run)) +
     ggplot2::geom_line() +
@@ -184,12 +210,12 @@ plot_retro <- function(mydir,  model_settings, output) {
     ggplot2::facet_wrap("Reference_Point", scales = 'free_y')
   ggplot2::ggsave(filename = file.path(retro_dir, "retro_percent_difference_4_panel.png"), width = 10, height = 12)
 
-  sub_out <- df_out[df_out$Reference_Point != "Recruits",]
-  find <- sub_out %>%
-    dplyr::filter(Yr >= xrange[1]) %>%
+  sub_out <- df_out[df_out[["Reference_Point"]] != "Recruits",]
+  find <- sub_out |>
+    dplyr::filter(Yr >= xrange[1]) |>
     dplyr::summarize(
       bound = abs(max(diff)))
-  yrange <- c(-1 * find$bound - 5, find$bound + 5)
+  yrange <- c(-1 * find[["bound"]] - 5, find[["bound"]] + 5)
 
   ggplot2::ggplot(sub_out, ggplot2::aes(x = Yr, y = diff, col = Run)) +
     ggplot2::geom_line() +
