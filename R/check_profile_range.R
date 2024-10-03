6c428e2bad713c2469d7391e0ece4eef496d0bce
Chantel Wetzel <chantel.wetzel@noaa.gov>
2024-10-03 08:28:16 -0700

    refactor: add additional checks for profiles

--- R/check_profile_range.R
+++ R/check_profile_range.R
@@ -13,38 +13,50 @@
 check_profile_range <- function(mydir, model_settings) {
   # Read in the base model
   rep <- r4ss::SS_output(
-    file.path(mydir, model_settings$base_name),
+    file.path(mydir, model_settings[["base_name"]]),
     covar = FALSE,
     printstats = FALSE,
     verbose = FALSE
   )
 
-  N <- nrow(model_settings$profile_details)
+  N <- nrow(model_settings[["profile_details"]])
   for (aa in 1:N) {
     profile_details <- model_settings[["profile_details"]][aa, ]
     para <- profile_details[, "parameters"]
-    est <- rep$parameters[rep$parameters$Label == para, "Value"]
+    if (!any(para == rep[["parameters"]][["Label"]])) {
+      cli::cli_abort(
+        "{para} does not match a parameter name in the model."
+      )
+    }
+    est <- rep[["parameters"]][rep[["parameters"]][["Label"]] == para, "Value"]
 
     # Determine the parameter range
-    if (profile_details$param_space == "relative") {
+    if (profile_details[["param_space"]] == "relative") {
       range <- c(
-        est + profile_details$low,
-        est + profile_details$high
+        est + profile_details[["low"]],
+        est + profile_details[["high"]]
       )
     }
-    if (profile_details$param_space == "multiplier") {
+    if (profile_details[["param_space"]] == "multiplier") {
       range <- c(
-        est - est * profile_details$low,
-        est + est * profile_details$high
+        est - est * profile_details[["low"]],
+        est + est * profile_details[["high"]]
       )
     }
-    if (profile_details$param_space == "real") {
+    if (profile_details[["param_space"]] == "real") {
       range <- c(
-        profile_details$low,
-        profile_details$high
+        profile_details[["low"]],
+        profile_details[["high"]]
       )
     }
-    step_size <- profile_details$step_size
+    step_size <- profile_details[["step_size"]]
+
+    if((max(range) - min(range)) < step_size) {
+      cli::cli_abort(
+        "The step size of {step_size} appears to be set too large to
+        profile over {para} from value of {range[1]} to {range[2]}."
+      )
+    }
 
     # Create parameter vect from base down and the base up
     if (est != round_any(est, step_size, f = floor)) {
@@ -66,8 +78,13 @@ check_profile_range <- function(mydir, model_settings) {
     }
 
     vec <- c(low, high)
-    cli::cli_inform(
-      "Profiling over {para} across values of {vec}."
-    )
+    if (est %in% vec) {
+      vec <- vec[!vec == est]
+    }
+    if(model_settings[["verbose"]]) {
+      cli::cli_inform(
+        "Profiling over {para} across values of {sort(vec)}."
+      )
+    }
   }
 }
--- R/run_diagnostics.R
+++ R/run_diagnostics.R
@@ -14,7 +14,6 @@
 run_diagnostics <- function(mydir, model_settings) {
   exe <- r4ss::check_exe(exe = model_settings$exe, dir = file.path(mydir, model_settings[["base_name"]]))[["exe"]]
   model_settings[["exe"]] <- exe
-  "%>%" <- magrittr::"%>%"
 
   # Check for Report file
   model_dir <- file.path(mydir, paste0(model_settings[["base_name"]]))
