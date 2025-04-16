dba3dd835420c85b7b44603c2516171f6b52708b
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2025-04-16 06:40:22 -0700

    add arg and change second figure
    The argument allows users to pass a gemm object to the function for non-coastwide models

--- R/compare_model_gemm_catch.R
+++ R/compare_model_gemm_catch.R
@@ -3,6 +3,14 @@
 #'
 #' @param  replist List of model objects created by `r4ss::SS_output()`.
 #' @param  common_name Common name of the species in the GEMM database.
+#' @param  gemm_data A data frame of GEMM data from `nwfscSurvey::pull_gemm()`.
+#'   If this argument is left blank, the function will pull the GEMM data
+#'   and evaluate mortality on a coastwide basis.  However, if your model
+#'   is not coastwide and the GEMM data can be split out to match the model
+#'   area (e.g., assessment split north and south of 40 10 N. latitude) this
+#'   filtered data frame can be passed to the function via this argument and
+#'   the total mortality comparisons will be done relative to that data object.
+#'   The default value is NULL.
 #' @param  dir Directory to save figure. If no directory is provided the
 #'   figure is not saved. If a `dir` is specified a figure of the model
 #'   estimated total mortality compared to the GEMM total mortality, a
@@ -19,11 +27,16 @@
 compare_model_gemm_catch <- function(
     replist,
     common_name,
+    gemm_data = NULL,
     dir = NULL,
     add_name = NULL,
     verbose = TRUE) {
   nwfscSurvey::check_dir(dir = dir, verbose = verbose)
   if (!is.null(add_name)) { add_name <- paste0("_", add_name)}
+  # Check for column name in the CATCH table based on SS3 version
+  if (any("kill_bio" %in% colnames(replist[["catch"]]))) {
+    colnames(replist[["catch"]])[colnames(replist[["catch"]]) == "kill_bio"] <- "dead_bio"
+  }
   catch <- replist[["catch"]] |>
     dplyr::select(Fleet_Name, Yr, sel_bio, dead_bio, ret_bio) |>
     dplyr::rename(year = Yr) |>
@@ -45,7 +58,10 @@ compare_model_gemm_catch <- function(
     )
   }
 
-  gemm <- nwfscSurvey::pull_gemm(common_name = common_name, dir = dir) |>
+  if (is.null(gemm_data)){
+    gemm_data <- nwfscSurvey::pull_gemm(common_name = common_name, dir = dir)
+  }
+  gemm <- gemm_data |>
     dplyr::group_by(year) |>
     dplyr::summarise(
       gemm_dead_discard = sum(total_discard_with_mort_rates_applied_mt),
@@ -76,17 +92,17 @@ compare_model_gemm_catch <- function(
   }
 
   gg2 <- ggplot2::ggplot() +
-    ggplot2::geom_bar(stat = "identity", data = catch,
-                      ggplot2::aes(x = year, y = dead_discard, fill = Fleet_Name), alpha = 0.75) +
+    ggplot2::geom_bar(stat = "identity", data = catch_gemm, ggplot2::aes(x = year, y = dead_discard, fill = Fleet_Name), alpha = 0.75) +
+    ggplot2::geom_line(data = gemm, ggplot2::aes(x = year, y = gemm_dead_discard), color = "black", linewidth = 2) +
     ggplot2::scale_fill_viridis_d() +
-    ggplot2::ylab("Total Dead Discard (mt)") +
+    ggplot2::ylab("Dead Discard (mt)") +
     ggplot2::xlab("Year") +
     ggplot2::theme_bw()
 
   if (!is.null(dir)){
     ggplot2::ggsave(
       gg2,
-      filename = file.path(dir, paste0("model_discard_mortality_all_years", add_name, ".png"))
+      filename = file.path(dir, paste0("model_gemm_dead_discard_comparison", add_name, ".png"))
     )
   } else {
     gg2
