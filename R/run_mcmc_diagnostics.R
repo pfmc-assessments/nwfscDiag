878c12250bc02e8517d0c22953fff367b87b4ba2
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2025-05-08 14:03:55 -0700

    add check to turn of using the par file

--- R/run_mcmc_diagnostics.R
+++ R/run_mcmc_diagnostics.R
@@ -56,9 +56,18 @@ run_mcmc_diagnostics <- function(
 
   # Check the recruitment option being used
   ctl <- r4ss::SS_readctl(file = file.path(dir_wd, "control.ss_new"))
-  if (ctl$do_recdev == 1) {
-    cli::cli_abort("Recruitment deviation option is set to 1 (zero-centered) in the control file.
-    In order to avoid bias in MCMCs this option needs to be set to 2.")
+  if (ctl$do_recdev != 2) {
+    cli::cli_abort("Recruitment deviation option in the control file must be set to option 2 in order to avoid bias in MCMCs.")
+  }
+  # Turn off read the par file if necessary
+  starter <- r4ss::SS_readstarter(file = file.path(dir_wd, "starter.ss"))
+  if (starter$init_values_src == 1) {
+    starter$init_values_src <- 0
+    r4ss::SS_writestarter(
+      mylist = starter,
+      dir = dir_wd,
+      overwrite = TRUE
+    )
   }
 
   # Run the model if need be
